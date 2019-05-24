eaab3fbafd4172002c55636ae0e7e1ee947d6542
gtouya <guillaume.touya@ign.fr>
2019-05-24 15:49:46 +0200

    first draft of HAL.js (only works for journals)

--- js/hal.js
+++ js/hal.js
@@ -26,18 +26,92 @@ var getAllPublicationsAuthor = function(halId){
   var request = new XMLHttpRequest();
 
   // Open a new connection, using the GET request on the URL endpoint
-  var url = "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:%22"+halId+"%22&wt=json";
+  var url = "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:%22"+halId+"%22&wt=json&fl=citationFull_s";
   request.open('GET', url, true);
   console.log(url);
   console.log(request.status);
-  request.onload = function () {
-    console.log("inside");
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
+  var parentJ = document.getElementById("pubJ");
+  var parentC = document.getElementById("pubC");
+  var parentB = document.getElementById("pubB");
+  var parentW = document.getElementById("pubW");
+  var parentO = document.getElementById("pubO");
 
-    data.forEach(docs => {
+  request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    console.log(data.response);
+    console.log(data.response.docs);
+    data.response.docs.forEach(docs => {
       // Log each doc id
-      console.log(movie.docid)
+      console.log(docs.docid)
     })
   };
+
+  request.send();
+}
+
+var getJournalPublicationsAuthor = function(halId){
+  // Create a request variable and assign a new XMLHttpRequest object to it.
+  var request = new XMLHttpRequest();
+
+  // Open a new connection, using the GET request on the URL endpoint
+  var url = "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:%22"+halId
+    +"%22&wt=json&fl=citationFull_s&fq=docType_s:\"ART\"&fl=producedDateY_i,halId_s&sort=producedDateY_i desc";
+  request.open('GET', url, true);
+  //console.log(url);
+
+  var parentJ = document.getElementById("pubJ");
+
+  request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    //console.log(data.response);
+    //console.log(data.response.docs);
+    data.response.docs.forEach(docs => {
+      // first create the list element with the citation
+      const listElement = document.createElement('li');
+      listElement.innerHTML = docs.citationFull_s
+      const appendChildElement = parentJ.appendChild(listElement);
+
+      // then create the "pubLink" div that contains the links related to the publication
+      pubLinkElement = document.createElement('div');
+      pubLinkElement.setAttribute("class","pubLink");
+      listElement.appendChild(pubLinkElement);
+      // create a span element inside the new div
+      spanElement = document.createElement('span');
+      spanElement.setAttribute("class","bibtex");
+      pubLinkElement.appendChild(spanElement);
+      // create an input element inside the span
+      inputElement = document.createElement('input');
+      inputElement.setAttribute("type","image");
+      inputElement.setAttribute("src","img/icons/bibtex.png");
+      inputElement.setAttribute("alt","BibTeX entry for this article");
+      inputElement.setAttribute("title","BibTeX entry for this article");
+      inputElement.setAttribute("onclick","window.open('bib/lobo_et_al_19.html','BibTex','width=800,height=200,top=100,left=100,scrollbars=yes,resizable=yes');");
+      spanElement.appendChild(inputElement);
+      // create an a element with the url to hal
+      aHALElement = document.createElement('a');
+      aHALElement.setAttribute("href",docs.halId_s);
+      aHALElement.setAttribute("class","imgLink");
+      imgElement = document.createElement('img');
+      imgElement.setAttribute("title","HAL");
+      imgElement.setAttribute("src","img/icons/hal.png");
+      imgElement.setAttribute("height","20");
+      imgElement.setAttribute("alt","HAL");
+      aHALElement.appendChild(imgElement);
+      pubLinkElement.appendChild(aHALElement);
+    })
+  };
+
+  request.send();
+}
+
+
+var sort_by = function(field, reverse, primer){
+   var key = function (x) {return primer ? primer(x[field]) : x[field]};
+
+   return function (a,b) {
+	  var A = key(a), B = key(b);
+	  return ( (A < B) ? -1 : ((A > B) ? 1 : 0) ) * [-1,1][+!!reverse];
+   }
 }
